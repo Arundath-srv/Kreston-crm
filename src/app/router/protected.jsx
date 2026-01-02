5cc907b6e1ad4a7604a7f033f21d132a70ab7fa8
Arundath-srv <arundath@srvithub.com>
2026-01-02 12:40:27 +0530

    UPDATE: favicon change

--- src/app/router/protected.jsx
+++ src/app/router/protected.jsx
@@ -24,7 +24,7 @@ const protectedRoutes = {
 
         {
           index: true,
-          element: <Navigate to="/client/add-client" />,
+          element: <Navigate to="/project/add-project" />,
         },
 
         {
